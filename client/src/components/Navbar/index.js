860d5f98727dd26e87cc6ce03046934469e415e1
BrittGall <crlnfllr@gmail.com>
2019-12-18 21:32:31 -0600

    links in navbar work

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,66 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      {/* <a className="navbar-brand" href="/">
-        Logo Goes HERE (img)
-      </a> */}
       <ul className="nav nav-tabs">
         <li className="nav-item">
-          <a
-            href="#home"
-            onClick={() => props.handlePageChange("Home")}
-            className="nav-link"
-          >
-            Home (or LOGO image?)
-          </a>
+          <Link to="/">Home (or LOGO image?)</Link>
         </li>
         <li className="nav-item">
-          <a
-            href="#login"
-            onClick={() => props.handlePageChange("Login")}
-            className="nav-link"
-          >
-            Sign In
-          </a>
+          <Link to="/Login"> Login</Link>
         </li>
         <li className="nav-item">
-          <a
-            href="#swaddle"
-            onClick={() => props.handlePageChange("Swaddle")}
-            className="nav-link"
-          >
-            Swaddle (Create a Meme)
-          </a>
+          <Link to="/Swaddle"> Swaddle (Create a Meme)</Link>
         </li>
         <li className="nav-item">
-          {/* set so this doesn't show unless logged in? */}
-          <a
-            href="#gallery"
-            onClick={() => props.handlePageChange("Gallery")}
-            className="nav-link"
-          >
-            My Meme Gallery
-          </a>
+          {/* if not logged in it askes you to login or register*/}
+          <Link to="/Gallery"> Gallery</Link>
         </li>
         <li className="nav-item">
-          <a
-            href="#donate"
-            onClick={() => props.handlePageChange("Donate")}
-            className="nav-link"
-          >
-            Donate
-          </a>
+          <Link to="/Donate"> Donate</Link>
         </li>
         <li className="nav-item">
-          <a
-            href="#about"
-            onClick={() => props.handlePageChange("About")}
-            className="nav-link"
-          >
-            About
-          </a>
+          <Link to="/About"> About</Link>
         </li>
       </ul>
     </nav>
