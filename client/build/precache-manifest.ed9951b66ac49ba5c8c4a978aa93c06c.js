a3612c25fd0628dceadefb121ed124b8d7f82e01
cecileanne <cecileannesison@gmail.com>
2019-12-18 21:40:23 -0600

    built client

---
+++ client/build/precache-manifest.ed9951b66ac49ba5c8c4a978aa93c06c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8b6c2766149cd1c1a2df8d0dce677a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d812d9f3e4c6794219a8",
+    "url": "/static/css/main.17232da6.chunk.css"
+  },
+  {
+    "revision": "75472d34f0ac19310d3b",
+    "url": "/static/js/2.b4d1c496.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.b4d1c496.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d812d9f3e4c6794219a8",
+    "url": "/static/js/main.97fe30a8.chunk.js"
+  },
+  {
+    "revision": "d85547e790bb47fc8f2e",
+    "url": "/static/js/runtime-main.783b24d2.js"
+  }
+]);
