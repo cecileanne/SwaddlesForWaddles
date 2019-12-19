b20b77d7a6e1677fe871c6f5d318c99c66d4437a
cecileanne <cecileannesison@gmail.com>
2019-12-18 21:48:50 -0600

    generalized port bind

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ mongoose.connect(
 );
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, "0.0.0.0", function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
