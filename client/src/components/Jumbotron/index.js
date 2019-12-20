bc658126786be2d63b3f23f98f6d3610c06c6764
BrittGall <crlnfllr@gmail.com>
2019-12-19 21:29:40 -0600

    basic jumbotron

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,8 +1,19 @@
-// import React from "react";
-// import "./style.css";
+import React from "react";
 
-// function swaddlePenguin(props) {
-//   return <div />;
-// }
+function Jumbotron({ children }) {
+  return (
+    <div
+      style={{
+        height: 300,
+        clear: "both",
+        paddingTop: 120,
+        textAlign: "center"
+      }}
+      className="jumbotron"
+    >
+      {children} {/* header on most pages, carousel in Swaddle */}
+    </div>
+  );
+}
 
-// export default swaddlePenguin;
+export default Jumbotron;
