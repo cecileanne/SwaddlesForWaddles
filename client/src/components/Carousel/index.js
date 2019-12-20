c1915708d6900c08f5862914d9ad7e44a452f3f3
BrittGall <crlnfllr@gmail.com>
2019-12-20 00:00:53 -0600

    carousel has basic function

--- client/src/components/Carousel/index.js
+++ client/src/components/Carousel/index.js
@@ -1,15 +1,105 @@
 import React from "react";
-import "./style.css";
+// import { Container } from "../../components/Grid";
+import InfiniteCarousel from "react-leaf-carousel";
 
-function Carousel(props) {
+const Carousel = () => {
   return (
-    <div className="input-group">
-      <div className="input-group-prepend">
-        <span className="Add to Image">Add to Image</span>
+    <InfiniteCarousel
+      breakpoints={[
+        {
+          breakpoint: 500,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3
+          }
+        }
+      ]}
+      dots={true}
+      showSides={true}
+      sidesOpacity={0.5}
+      sideSize={0.1}
+      slidesToScroll={4}
+      slidesToShow={4}
+      scrollOnDevice={true}
+    >
+      <div>
+        <img
+          data-name=""
+          alt=""
+          src={
+            process.env.PUBLIC_URL + "/assets/images/penguins/penguinTest1.jpg"
+          }
+        />
       </div>
-      <textarea className="form-control" aria-label="Add to Image"></textarea>
-    </div>
+      <div>
+        <img
+          data-name=""
+          alt=""
+          src={
+            process.env.PUBLIC_URL +
+            "/assets/images/sweaters/blueSweaterTest.png"
+          }
+        />
+      </div>
+
+      <div>
+        <img
+          data-name=""
+          alt=""
+          src={
+            process.env.PUBLIC_URL + "/assets/images/penguins/penguinTest1.jpg"
+          }
+        />
+      </div>
+      <div>
+        <img
+          data-name=""
+          alt=""
+          src={
+            process.env.PUBLIC_URL +
+            "/assets/images/sweaters/blueSweaterTest.png"
+          }
+        />
+      </div>
+
+      <div>
+        <img
+          data-name=""
+          alt=""
+          src={
+            process.env.PUBLIC_URL + "/assets/images/penguins/penguinTest1.jpg"
+          }
+        />
+      </div>
+      <div>
+        <img
+          data-name=""
+          alt=""
+          src={
+            process.env.PUBLIC_URL +
+            "/assets/images/sweaters/blueSweaterTest.png"
+          }
+        />
+      </div>
+      <div>
+        <img
+          data-name=""
+          alt=""
+          src={
+            process.env.PUBLIC_URL +
+            "/assets/images/sweaters/redSweaterTest.png"
+          }
+        />
+      </div>
+    </InfiniteCarousel>
   );
-}
+};
 
 export default Carousel;
