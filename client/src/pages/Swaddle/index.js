5846e990978e51ce6b66645c8ffc980725c189dc
BrittGall <crlnfllr@gmail.com>
2019-12-21 00:04:53 -0600

    editing meme creation area

--- client/src/pages/Swaddle/index.js
+++ client/src/pages/Swaddle/index.js
@@ -4,8 +4,7 @@ import UserTextInput from "../../components/UserTextInput";
 import { ShowPenguin, ShowSweater } from "../../components/ButtonIcon";
 import { ResetBtn, SaveBtn } from "../../components/ButtonSubmit";
 import Carousel from "../../components/Carousel";
-
-// import "./style.css";
+import "./style.css";
 
 function Swaddle() {
   return (
@@ -34,22 +33,21 @@ function Swaddle() {
             </Row>
           </Col>
           <Col size="md-6">
-            {/* Not sure if figure is best image area */}
-            <figure className="figure">
-              <img
-                src={
-                  process.env.PUBLIC_URL +
-                  "/assets/images/sweaters/blueSweaterTest.png"
-                }
-                className="figure-img img-fluid rounded"
-                alt="..."
-              />
-              <figcaption className="figure-caption">
-                meme area (delete this figcaption)
-              </figcaption>
-            </figure>
+            <section>
+              <div className="card">
+                <img
+                  className="card-img-top"
+                  src={
+                    process.env.PUBLIC_URL +
+                    "/assets/images/penguins/penguinTest1.jpg"
+                  }
+                  alt="Card image cap"
+                />
+              </div>
+            </section>
           </Col>
         </Row>
+
         <Row>
           <Col size="md-4 offset-md-2">
             <SaveBtn />
