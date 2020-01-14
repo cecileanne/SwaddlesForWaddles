eb54bcf328c5f4ea4586b4e904f769076379ebe4
Rosalis Freebairn <rfreeb@artic.edu>
2020-01-13 18:37:03 -0600

    Delete remaining colunm/row tags in home page js.

--- client/src/pages/About/index.js
+++ client/src/pages/About/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Col, Row, Container } from "../../components/Grid";
-import Jumbotron from "../../components/Jumbotron";
+// import Jumbotron from "../../components/Jumbotron";
 import { DonateBtn } from "../../components/ButtonSubmit";
 import Navbar from "../../components/Navbar";
 import "./style.css";
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,18 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Col, Row, Container } from "../../components/Grid";
-import Jumbotron from "../../components/Jumbotron";
+// import { Col, Row, Container } from "../../components/Grid";
+// import Jumbotron from "../../components/Jumbotron";
 import Navbar from "../../components/Navbar";
 import "./style.css";
 
 function Home() {
   return (
-    // <Container fluid>
-    //   <Row>
-    //     <Col size="md-9">
-    //       <Row>
-    //         <Col size="md-12">
-
     <div className="background">
       <Navbar className="navbar" />
       <div className="logo-wrapper">
@@ -36,44 +30,6 @@ function Home() {
         <p className="brand-subtext">Memes for a Cause</p>
       </div>
     </div>
-    //   </Col>
-    // </Row>
-
-    //   {/* <Row>
-    //     <Col size="md-12">
-    //       <article>
-    //         <p>
-    //           A group of penguins is called a waddle, and a sweater for a
-    //           penguin is like a swaddle. Here, on Swaddles for Waddles, you
-    //           can create your own images and memes of penguins in sweaters!
-    //         </p>
-    //         <p>Create fun memes and be as fun and silly as you want.</p>
-    //       </article>
-    //     </Col>
-    //   </Row> */}
-    //   {/* <Row>
-    //     <Link to="/About">
-    //       <a className="btn btn-primary" role="button">
-    //         About
-    //       </a>
-    //     </Link>
-    //     <Link to="/Swaddle">
-    //       <a className="btn btn-primary" role="button">
-    //         Swaddle a Pengiun!
-    //       </a>
-    //     </Link>
-    //     <Link to="/Donate">
-    //       <a className="btn btn-primary" role="button">
-    //         Donate
-    //       </a>
-    //     </Link>
-    //   </Row> */}
-    // </Col>
-    // <Col size="md-3">
-
-    //     </Col>
-    //   </Row>
-    // </Container>
   );
 }
 
