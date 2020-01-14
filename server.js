5bf021af2dedf8eef76906936dbc8c1491b3c298
cecileanne <cecileannesison@gmail.com>
2020-01-13 22:35:07 -0600

    refractored express.static method

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 // app.use(logger("dev"));
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client/build")));
 }
 
 // Add routes, both API and view
