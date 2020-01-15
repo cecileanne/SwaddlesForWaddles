6052997fb0fe2b5d083583b313dfe3c57d0881ac
cecileanne <cecileannesison@gmail.com>
2020-01-14 18:26:21 -0600

    updating the div classNames for styling on the donation page

--- client/src/pages/Donate/index.js
+++ client/src/pages/Donate/index.js
@@ -31,11 +31,7 @@ class Donate extends Component {
         .then(res => {
           this.setState(
             {
-<<<<<<< HEAD
-              donation: res.data,
-=======
               donations: res.data,
->>>>>>> 7b79913a76f830fb78500f923d9ba241632c91b9
               amount: ""
             },
             () => console.log("STATE MOUNTS", this.state)
@@ -70,7 +66,6 @@ class Donate extends Component {
       })
       .catch(err => console.log(err));
   };
-
   render() {
     return (
       <div className="donate">
@@ -83,7 +78,7 @@ class Donate extends Component {
                   process.env.PUBLIC_URL +
                   "/assets/images/icons/swaddles_for_waddles_logo.png"
                 }
-                alt="Swaddles for Waddles Logo"
+                alt="Swaddles for Waddles logo"
               ></img>
               <h3 className="donationGreet">Hello {this.state.userName}!</h3>
             </Col>
@@ -91,13 +86,14 @@ class Donate extends Component {
               <h1 className="donationTitle">Support the Penguins</h1>
             </Col>
             <Col size="md-1">
-               <Navbar className="navBarBlue"></Navbar>
+              <Navbar className="navBarBlue"></Navbar>
             </Col>
           </Row>
         </div>
+
         <div className="containerDonationReq">
           <Row>
-            <Col size="md-8" classname="containerInfoForm">
+            <Col size="md-8" className="containerInfoForm">
               <h4>
                 The World Wildlife Fund is our favorite organization for saving
                 animals. If you would like to help the penguins please pledge to
@@ -107,7 +103,6 @@ class Donate extends Component {
                 Please note - no financial information will be requested at this
                 time.
               </h4>
-              <hr></hr>
               <form>
                 <Input
                   value={this.state.value}
@@ -125,16 +120,7 @@ class Donate extends Component {
                 </div>
               </form>
             </Col>
-            <Col size="md-6">
-<<<<<<< HEAD
-              {this.state.donations.map(donation => (
-                <li className="list-group-item">
-                  {donation.transactionDate} | {donation.amount}
-                </li>
-              ))}
-              <div>
-                <h2>Total Donations to date: $50.00 (MAKE THIS INTO STATE?)</h2>
-=======
+            <Col size="md-4" className="containerForTable">
               <List k={this.state.donations} />
               {this.state.donations.map(donation => (
                 <li className="list-group-item">
@@ -143,17 +129,16 @@ class Donate extends Component {
               ))}
               <div>
                 {this.state.donations.length ? (
-                  <h2>
-                    Total: $
+                  <h3>
+                    Your donations to date: $
                     {this.state.donations.reduce(
                       (total, object) => total + Number(object.amount),
                       0
                     )}
-                  </h2>
+                  </h3>
                 ) : (
-                  <p>no donations</p>
+                  <p>No donations-- Please give generously</p>
                 )}
->>>>>>> 7b79913a76f830fb78500f923d9ba241632c91b9
               </div>
             </Col>
           </Row>
